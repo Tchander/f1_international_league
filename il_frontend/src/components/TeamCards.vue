c0347bf63f4be0283e8ed34d2ceb60a40b8af524
Tchander <evgeniy.tchander@yandex.ru>
2021-09-10 15:53:26 +0300

    develop: feat: add some pilot info to TeamCards

--- il_frontend/src/components/TeamCards.vue
+++ il_frontend/src/components/TeamCards.vue
@@ -15,6 +15,33 @@
           class="il-pilot-card__image"
           :src="$options.API_MEDIA_URL + pilot.image"
         ></v-img>
+        <div class="il-pilot-card__info">
+          Лига: <span>{{ pilot.league }}</span>
+        </div>
+        <div v-if="pilot.total_score % 1 !== 0" class="il-pilot-card__info">
+          Очков в личном зачёте: <span>{{ pilot.total_score }}</span>
+        </div>
+        <div v-else class="il-pilot-card__info">
+          Очков в личном зачёте: <span>{{ parseInt(pilot.total_score) }}</span>
+        </div>
+        <div class="il-pilot-card__info">
+          Лучший результат в гонке: <span>{{ pilot.best_race_finish }}</span>
+        </div>
+        <div class="il-pilot-card__info">
+          Лучший результат в квалификации:
+          <span>{{ pilot.highest_grid_position }}</span>
+        </div>
+        <div class="il-pilot-card__info">
+          Побед в гонке над напарником:
+          <span>{{ pilot.race_victories_over_teammate }}</span>
+        </div>
+        <div class="il-pilot-card__info">
+          Побед в квалификации над напарником:
+          <span>{{ pilot.qualifying_victories_over_teammate }}</span>
+        </div>
+        <div class="il-pilot-card__info">
+          Количество сходов: <span>{{ pilot.do_not_finish }}</span>
+        </div>
       </v-card-text>
     </v-card>
   </div>
@@ -76,12 +103,21 @@ export default {
   border-radius: 4px;
   padding: 2px 5px;
 }
-
 .il-pilot-card__image.il-pilot-card__image.il-pilot-card__image {
   width: 178px;
   height: 205px;
-  margin: 0 auto;
+  margin: 0 auto 20px auto;
   border-radius: 10px;
   box-shadow: 0 0 8px rgb(92, 25, 48);
+}
+.il-pilot-card__info {
+  color: #fff;
+  font-size: 18px;
+  margin-bottom: 10px;
+}
+.il-pilot-card__info > span {
+  color: rgb(23, 123, 222);
+  font-weight: 700;
+  font-size: 20px;
 }
 </style>
